01282c16f37e305e544dc59f26a1f42b28f7334b
Daniel Hufnagl <d.hufnagl@codelisk.com>
2026-07-15 14:14:58 +0200

    fix(api): PDF-Fotos korrekt drehen und sortieren

--- src/api/tests/UnitTests/Heimatplatz.Api.Core.UnitTests/Features/ForeclosureAuctions/ForeclosureImageServiceTests.cs
+++ src/api/tests/UnitTests/Heimatplatz.Api.Core.UnitTests/Features/ForeclosureAuctions/ForeclosureImageServiceTests.cs
@@ -19,11 +19,7 @@ public class ForeclosureImageServiceTests
     [Test]
     public void ExtractPdfImageCandidates_ReadsEmbeddedPhotoAtOriginalResolution()
     {
-        var service = new ForeclosureImageService(
-            Substitute.For<IHttpClientFactory>(),
-            Substitute.For<IWebHostEnvironment>(),
-            Options.Create(new ScrapingOptions()),
-            NullLogger<ForeclosureImageService>.Instance);
+        var service = CreateService();
 
         var candidates = service.ExtractPdfImageCandidates(CreatePdfWithPhoto());
         var selected = ForeclosureImageSelector.SelectPdfImages(candidates, maxCount: 20);
@@ -36,6 +32,51 @@ public class ForeclosureImageServiceTests
         selected[0].Extension.Should().Be(".jpg");
     }
 
+    [Test]
+    public void ExtractPdfImageCandidates_SortsImagesInVisualReadingOrder()
+    {
+        var bottomPhoto = CreatePhoto(seed: 41);
+        var topPhoto = CreatePhoto(seed: 42);
+        var builder = new PdfDocumentBuilder();
+        builder.AddPage(PageSize.A4);
+        var page = builder.AddPage(PageSize.A4);
+        page.AddJpeg(bottomPhoto, new PdfRectangle(20, 20, 560, 390));
+        page.AddJpeg(topPhoto, new PdfRectangle(20, 430, 560, 800));
+
+        var candidates = CreateService().ExtractPdfImageCandidates(builder.Build());
+
+        candidates.Should().HaveCount(2);
+        candidates[0].Bytes.Should().Equal(topPhoto);
+        candidates[1].Bytes.Should().Equal(bottomPhoto);
+    }
+
+    [Test]
+    public void ApplyPdfRotation_RotatesPositiveAngleCounterClockwise()
+    {
+        var sourceBytes = CreateOrientationPng();
+
+        var result = ForeclosureImageService.ApplyPdfRotation(
+            sourceBytes,
+            ".png",
+            width: 4,
+            height: 2,
+            rotationDegrees: 90);
+
+        result.Width.Should().Be(2);
+        result.Height.Should().Be(4);
+        using var rotated = SKBitmap.Decode(result.Bytes);
+        rotated.GetPixel(0, 0).Should().Be(SKColors.Green);
+        rotated.GetPixel(1, 0).Should().Be(SKColors.Yellow);
+        rotated.GetPixel(0, 3).Should().Be(SKColors.Red);
+        rotated.GetPixel(1, 3).Should().Be(SKColors.Blue);
+    }
+
+    private static ForeclosureImageService CreateService() => new(
+        Substitute.For<IHttpClientFactory>(),
+        Substitute.For<IWebHostEnvironment>(),
+        Options.Create(new ScrapingOptions()),
+        NullLogger<ForeclosureImageService>.Instance);
+
     private static byte[] CreatePdfWithPhoto()
     {
         var builder = new PdfDocumentBuilder();
@@ -45,10 +86,10 @@ public class ForeclosureImageServiceTests
         return builder.Build();
     }
 
-    private static byte[] CreatePhoto()
+    private static byte[] CreatePhoto(int seed = 42)
     {
         using var bitmap = new SKBitmap(780, 520);
-        var random = new Random(42);
+        var random = new Random(seed);
         for (var y = 0; y < bitmap.Height; y++)
         {
             for (var x = 0; x < bitmap.Width; x++)
@@ -62,6 +103,23 @@ public class ForeclosureImageServiceTests
 
         using var image = SKImage.FromBitmap(bitmap);
         using var data = image.Encode(SKEncodedImageFormat.Jpeg, 90);
+        return data.ToArray();
+    }
+
+    private static byte[] CreateOrientationPng()
+    {
+        using var bitmap = new SKBitmap(4, 2);
+        bitmap.SetPixel(0, 0, SKColors.Red);
+        bitmap.SetPixel(1, 0, SKColors.Red);
+        bitmap.SetPixel(2, 0, SKColors.Green);
+        bitmap.SetPixel(3, 0, SKColors.Green);
+        bitmap.SetPixel(0, 1, SKColors.Blue);
+        bitmap.SetPixel(1, 1, SKColors.Blue);
+        bitmap.SetPixel(2, 1, SKColors.Yellow);
+        bitmap.SetPixel(3, 1, SKColors.Yellow);
+
+        using var image = SKImage.FromBitmap(bitmap);
+        using var data = image.Encode(SKEncodedImageFormat.Png, 100);
         return data.ToArray();
     }
 }
